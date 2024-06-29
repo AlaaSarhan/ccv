5247bf94e22324dad7cd9d89ca261bbdce06d5ad
Alaa Sarhan <alaa.sarhan@wundertax.com>
2024-06-29 19:58:26 +0200

    improvements with 0.5 and styling

--- src/App.jsx
+++ src/App.jsx
@@ -14,22 +14,25 @@ const flipPattern = (pattern) => {
 }
 
 function App() {
-  const [otherParentDays, setOtherParentDays] = useState(
-    JSON.parse(localStorage.getItem('otherParentDays')) || [ [], [], [], [], [], [], [], [], [], [], [], [] ]
-  );
+  const [otherParentDays, setOtherParentDays] = useState([]);
 
   useEffect(() => {
-    const storedOtherParentDays = JSON.parse(localStorage.getItem('otherParentDays'));
-    
-    if (storedOtherParentDays) {
-      setOtherParentDays(storedOtherParentDays);
-      console.log('loaded');
+    if (otherParentDays.legnth) {
+      return
     }
+
+    const storedOtherParentDays = JSON.parse(localStorage.getItem('otherParentDays'));
+   
+    setOtherParentDays(storedOtherParentDays.length ? storedOtherParentDays : [ [], [], [], [], [], [], [], [] ]);
   }, []);
 
   useEffect(() => {
+    if (!otherParentDays.length) {
+      return;
+    }
+
+    console.log('storing', otherParentDays)
     localStorage.setItem('otherParentDays', JSON.stringify(otherParentDays));
-    console.log('saved');
   }, [otherParentDays]);
 
   const reset = useCallback(() => {
@@ -47,10 +50,35 @@ function App() {
     setOtherParentDays(newOtherParentsDays);
   }, []);
 
+  const set = useCallback(() => {
+    const value = JSON.parse(window.prompt("Enter json value you have copied/saved.", '[ [], [], [], [] ]'));
+
+    if (!value) {
+      window.alert('Invalid value');
+      return;
+    }
+
+    setOtherParentDays(value);
+
+  })
+
   const toggleDay = useCallback((weekIdx, dayIdx) => {
     const newOtherParentsDays = [...otherParentDays];
     newOtherParentsDays[weekIdx] = [...(otherParentDays[weekIdx] || [])];
-    newOtherParentsDays[weekIdx][dayIdx] = newOtherParentsDays[weekIdx][dayIdx] == 1 ? 0 : 1;
+    const oldValue = newOtherParentsDays[weekIdx][dayIdx];
+    let newValue;
+    switch (oldValue) {
+      case 1:
+        newValue = 0;
+        break;
+      case 0.5:
+        newValue = 1;
+        break;
+      default:
+        newValue = 0.5;
+        break;
+    }
+    newOtherParentsDays[weekIdx][dayIdx] = newValue;
     setOtherParentDays(newOtherParentsDays);
   }, [otherParentDays]);
 
@@ -75,15 +103,35 @@ function App() {
 
   }, [otherParentDays])
 
-  const totalShare = otherParentDays.length * 7.0;
-  const otherParentShare = (otherParentDays.reduce((sum, week) => sum + week.filter(Boolean).length, 0) / totalShare);
+  const totalDays = otherParentDays.length * 7.0;
+  const otherParentShare = (otherParentDays.reduce((sum, week) => sum + week.reduce((sum, day) => sum + day || 0, 0), 0) / totalDays);
   const parentShare = 1 - otherParentShare;
+
+  const totalEvenWeekendDays = Math.floor(otherParentDays.length / 2) * 2;
+  const otherParentEvenWeekendShare = (otherParentDays.filter((_, weekIdx) => (weekIdx + 1) % 2 == 0).reduce((sum, week) => sum + week.filter((_, dayIdx) => dayIdx > 4).reduce((sum, day) => sum + day, 0), 0)) / totalEvenWeekendDays;
+  const parentEvenWeekendShare = 1 - otherParentEvenWeekendShare;
+  
+  const totalOddWeekendDays = Math.ceil(otherParentDays.length / 2) * 2;
+  const otherParentOddWeekendShare = (otherParentDays.filter((_, weekIdx) => weekIdx % 2 == 0).reduce((sum, week) => sum + week.filter((_, dayIdx) => dayIdx > 4).reduce((sum, day) => sum + day, 0), 0)) / totalEvenWeekendDays;
+  const parentOddWeekendShare = 1 - otherParentOddWeekendShare;
+
 
   return (
     <>
-      <div>
+      <div className='cp'>
         <center>
-          <button onClick={reset}>Reset</button>
+          <div className='cp-value'>
+            <h2>Current value</h2>
+            <p>you can copy and save this value, and use the 'Set' button above to reset to it</p>
+            <form>
+              <input readonly={true} value={JSON.stringify(otherParentDays)} />
+            </form>
+          </div>
+
+          <div className='cp-buttons'>
+            <button onClick={reset}>Reset</button>
+            <button onClick={set}>Set</button>
+          </div>
         </center>
       </div>
       <div>
@@ -105,13 +153,13 @@ function App() {
             {
               otherParentDays.map((week, idx) => (<tr key={`week-no-${idx+1}`} className={`week-${(idx+1) % 2 == 0 ? 'even' : 'odd'}`}>
                 <td className="week-no-cell" onClick={() => repeatPattern(idx)}>{idx + 1}</td>
-                <td className={`weekday-cell mon-cell ${week[0] && 'other-parent-cell'}`} onClick={() => toggleDay(idx, 0)}></td>
-                <td className={`weekday-cell tue-cell ${week[1] && 'other-parent-cell'}`} onClick={() => toggleDay(idx, 1)}></td>
-                <td className={`weekday-cell wed-cell ${week[2] && 'other-parent-cell'}`} onClick={() => toggleDay(idx, 2)}></td>
-                <td className={`weekday-cell thu-cell ${week[3] && 'other-parent-cell'}`} onClick={() => toggleDay(idx, 3)}></td>
-                <td className={`weekday-cell fri-cell ${week[4] && 'other-parent-cell'}`} onClick={() => toggleDay(idx, 4)}></td>
-                <td className={`weekend-cell sat-cell ${week[5] && 'other-parent-cell'}`} onClick={() => toggleDay(idx, 5)}></td>
-                <td className={`weekend-cell sun-cell ${week[6] && 'other-parent-cell'}`} onClick={() => toggleDay(idx, 6)}></td>
+                <td className={'weekday-cell mon-cell ' + (week[0] > 0 && `other-parent-cell-${week[0]}`)} onClick={() => toggleDay(idx, 0)}></td>
+                <td className={'weekday-cell tue-cell ' + (week[1] > 0 && `other-parent-cell-${week[1]}`)} onClick={() => toggleDay(idx, 1)}></td>
+                <td className={'weekday-cell wed-cell ' + (week[2] > 0 && `other-parent-cell-${week[2]}`)} onClick={() => toggleDay(idx, 2)}></td>
+                <td className={'weekday-cell thu-cell ' + (week[3] > 0 && `other-parent-cell-${week[3]}`)} onClick={() => toggleDay(idx, 3)}></td>
+                <td className={'weekday-cell fri-cell ' + (week[4] > 0 && `other-parent-cell-${week[4]}`)} onClick={() => toggleDay(idx, 4)}></td>
+                <td className={'weekend-cell sat-cell ' + (week[5] > 0 && `other-parent-cell-${week[5]}`)} onClick={() => toggleDay(idx, 5)}></td>
+                <td className={'weekend-cell sun-cell ' + (week[6] > 0 && `other-parent-cell-${week[6]}`)} onClick={() => toggleDay(idx, 6)}></td>
               </tr>))
             }
           </tbody>
@@ -120,12 +168,32 @@ function App() {
      <div>
         <h2>Stats</h2>
 
-        <div>
-          <h3>Time share</h3>
-          <p>First parent time share: {Math.round(parentShare * 100)}%</p>
-          <p>Second parent time share: {Math.round(otherParentShare * 100)}%</p>
-        </div>
-        
+        <center>
+        <table>
+          <thead>
+            <tr>
+              <td></td>
+              <td>Total time share</td>
+              <td>Even weekends time share</td>
+              <td>Odd weekends time share</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>First parent</td>
+              <td>{Math.round(parentShare * 100)}%</td>
+              <td>{Math.round(parentEvenWeekendShare * 100)}%</td>
+              <td>{Math.round(parentOddWeekendShare * 100)}%</td>
+            </tr>
+            <tr style={{ backgroundColor: '#333' }}>
+              <td>Other parent (Shaded)</td>
+              <td>{Math.round(otherParentShare * 100)}%</td>
+              <td>{Math.round(otherParentEvenWeekendShare * 100)}%</td>
+              <td>{Math.round(otherParentOddWeekendShare * 100)}%</td>
+            </tr>
+          </tbody>
+        </table>
+        </center>
       </div>
     </>
   )
